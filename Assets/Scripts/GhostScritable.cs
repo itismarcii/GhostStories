b000726cab7eb7a8109f743e62332359e7595483
itismarcii <58007324+itismarcii@users.noreply.github.com>
2021-07-23 17:50:06 +0200

    Ghost Scriptable Version 1

--- Assets/Scripts/GhostScritable.cs
+++ Assets/Scripts/GhostScritable.cs
@@ -3,10 +3,39 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Ghost", menuName = "Ghost")]
 public class GhostScritable : ScriptableObject
 {
-    public enum Ghosts
+    public class Sanity
     {
-
+        public float loseTime;
+        public float loseAmount;
     }
-    public Ghosts ghost;
-    public Movement movement;
+
+    public class Abilities
+    {
+        public bool teleport = false;
+        public bool scream = false;
+        public bool lightActivity = false;
+        public bool objectInteraction = false;
+        public bool appearanceFlicker = false;
+        public bool charge = false;
+    }
+
+    public enum Mode
+    {
+        passive,
+        present,
+        active,
+        aggressive,
+        hunting
+    }
+
+    public enum Ghost
+    {
+        standard
+    }
+
+    public Ghost ghost;
+    public Sanity sanity;
+    public Mode mode;
+    public Abilities abilities;
+    [HideInInspector] public Movement movement;
 }
